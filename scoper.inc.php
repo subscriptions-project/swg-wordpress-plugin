eddb1690729d45ad9ada3cf436e1bb478c458ae5
Chris Antaki <cfa@google.com>
2020-01-03 12:53:12 -0800

    Branches scoper PHP file from Site Kit

--- scoper.inc.php
+++ scoper.inc.php
@@ -1,86 +1,96 @@
 <?php
-
-declare(strict_types=1);
-
+/**
+ * PHP-Scoper configuration file.
+ *
+ * @package   Subscriptions_Project\WordPress
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
 use Isolated\Symfony\Component\Finder\Finder;
-
-return [
-    // The prefix configuration. If a non null value will be used, a random prefix will be generated.
-    'prefix' => null,
-
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
-    // For more see: https://github.com/humbug/php-scoper#finders-and-paths
-    'finders' => [
-        Finder::create()->files()->in('src'),
-        Finder::create()
-            ->files()
-            ->ignoreVCS(true)
-            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
-            ->exclude([
-                'doc',
-                'test',
-                'test_old',
-                'tests',
-                'Tests',
-                'vendor-bin',
-            ])
-            ->in('vendor'),
-        Finder::create()->append([
-            'composer.json',
-        ]),
-    ],
-
-    // Whitelists a list of files. Unlike the other whitelist related features, this one is about completely leaving
-    // a file untouched.
-    // Paths are relative to the configuration file unless if they are already absolute
-    'files-whitelist' => [
-        'src/a-whitelisted-file.php',
-    ],
-
-    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
-    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
-    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
-    // heart contents.
-    //
-    // For more see: https://github.com/humbug/php-scoper#patchers
-    'patchers' => [
-        function (string $filePath, string $prefix, string $contents): string {
-            // Change the contents here.
-
-            return $contents;
-        },
-    ],
-
-    // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
-    // may want to share a common API between the bundled code of your PHAR and the consumer code. For example if
-    // you have a PHPUnit PHAR with isolated code, you still want the PHAR to be able to understand the
-    // PHPUnit\Framework\TestCase class.
-    //
-    // A way to achieve this is by specifying a list of classes to not prefix with the following configuration key. Note
-    // that this does not work with functions or constants neither with classes belonging to the global namespace.
-    //
-    // Fore more see https://github.com/humbug/php-scoper#whitelist
-    'whitelist' => [
-        // 'PHPUnit\Framework\TestCase',   // A specific class
-        // 'PHPUnit\Framework\*',          // The whole namespace
-        // '*',                            // Everything
-    ],
-
-    // If `true` then the user defined constants belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-constants' => true,
-
-    // If `true` then the user defined classes belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-classes' => true,
-
-    // If `true` then the user defined functions belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-functions' => true,
-];
+// Google API services to include classes for.
+$google_services = implode(
+	'|',
+	array(
+		'Analytics',
+		'AnalyticsReporting',
+		'AdSense',
+		'Pagespeedonline',
+		'PeopleService',
+		'SiteVerification',
+		'TagManager',
+		'Webmasters',
+	)
+);
+return array(
+	'prefix'                     => 'Google\\Site_Kit_Dependencies',
+	'finders'                    => array(
+		// General dependencies, except Google API services.
+		Finder::create()
+			->files()
+			->ignoreVCS( true )
+			->notName( '/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.(json|lock)/' )
+			->exclude(
+				array(
+					'doc',
+					'test',
+					'test_old',
+					'tests',
+					'Tests',
+					'vendor-bin',
+				)
+			)
+			->path( '#^firebase/#' )
+			->path( '#^google/apiclient/#' )
+			->path( '#^google/auth/#' )
+			->path( '#^guzzlehttp/#' )
+			->path( '#^monolog/#' )
+			->path( '#^psr/#' )
+			->path( '#^ralouphie/#' )
+			->path( '#^react/#' )
+			->in( 'vendor' ),
+		// Google API service infrastructure classes.
+		Finder::create()
+			->files()
+			->ignoreVCS( true )
+			->notName( '/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/' )
+			->exclude(
+				array(
+					'doc',
+					'test',
+					'test_old',
+					'tests',
+					'Tests',
+					'vendor-bin',
+				)
+			)
+			->path( "#^google/apiclient-services/src/Google/Service/($google_services)/#" )
+			->in( 'vendor' ),
+		// Google API service entry classes.
+		Finder::create()
+			->files()
+			->ignoreVCS( true )
+			->name( "#($google_services)\.php#" )
+			->in( 'vendor/google/apiclient-services/src/Google/Service' ),
+	),
+	'files-whitelist'            => array(
+		// This dependency is a global function which should remain global.
+		'vendor/ralouphie/getallheaders/src/getallheaders.php',
+	),
+	'patchers'                   => array(
+		function( $file_path, $prefix, $contents ) {
+			if ( preg_match( '#google/apiclient/src/Google/Http/REST\.php$#', $file_path ) ) {
+				$contents = str_replace( "\\$prefix\\intVal", '\\intval', $contents );
+			}
+			if ( false !== strpos( $file_path, 'vendor/google/apiclient-services/' ) ) {
+				$contents = str_replace( "'Google_Service_", "'" . $prefix . '\Google_Service_', $contents );
+				$contents = str_replace( '"Google_Service_', '"' . $prefix . '\Google_Service_', $contents );
+			}
+			return $contents;
+		},
+	),
+	'whitelist'                  => array(),
+	'whitelist-global-constants' => false,
+	'whitelist-global-classes'   => false,
+	'whitelist-global-functions' => false,
+);
