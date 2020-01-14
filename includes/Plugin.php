3fd73abeab9f45b06d4d36831321b7001f861e20
Chris <cfa@google.com>
2020-01-14 22:21:54 +0000

    Adds filter

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -12,6 +12,9 @@ namespace SubscribeWithGoogle\WordPress;
 /** Main class for the plugin. */
 final class Plugin {
 
+	/** Namespace for SwG variables. */
+	const SWG_NAMESPACE = 'SubscribeWithGoogle_';
+
 	/**
 	 * Main instance of the plugin.
 	 *
@@ -58,10 +61,65 @@ final class Plugin {
 	 *
 	 * @param array[string]string $atts Attributes affecting shortcode.
 	 */
-	private function shortcode_subscribe( $atts = [] ) {
+	public function shortcode_subscribe( $atts = [] ) {
 		$play_offers = $atts['play-offers'] ? $atts['play-offers'] : '';
 
 		return '<button class="swg-button" data-play-offers="' . $play_offers . '"></button>';
+	}
+
+	/**
+	 * Filters content on Post view pages.
+	 *
+	 * @param string $content Initial content of Post.
+	 * @return string Filtered content of Post.
+	 */
+	public function filter_the_content( $content ) {
+		// Check if we're inside the main loop in a single post page.
+		if ( ! is_single() || ! in_the_loop() || ! is_main_query() ) {
+			return $content;
+		}
+
+		// Verify this post is supposed to be locked, even.
+		// If it's free, just bail.
+		$products_str = trim( get_option( $this::SWG_NAMESPACE . 'products' ) );
+		if ( ! $products_str ) {
+			return $content;
+		}
+		$products = array_map(
+			function( $product ) {
+				// TODO: Create a utility method that does this.
+				return trim( $product );
+			},
+			explode( "\n", $products_str )
+		);
+		$meta_key = $this::SWG_NAMESPACE . 'product';
+		$product  = get_post_meta( get_the_ID(), $meta_key, true );
+		if ( ! $product || ! in_array( $product, $products ) ) {
+			return $content;
+		}
+
+		$more_tag         = '<span id="more-' . get_the_ID() . '"></span>';
+		$content_segments = explode( $more_tag, $content );
+
+		// Add Paywall wrapper & prompt.
+		if ( count( $content_segments ) > 1 ) {
+			$content_segments[1] = '
+<p class="swg-paywall-prompt">
+	🔒 <span>Subscribe to unlock the rest of this article.</span>
+	<br />
+	<br />
+	<button class="swg-button"></button>
+</p>
+
+<div class="swg-paywall">
+' . $content_segments[1] . '
+</div>
+';
+		}
+
+		$content = implode( $more_tag, $content_segments );
+
+		return $content;
 	}
 
 	/** Adds admin menu item. */
