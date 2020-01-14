451f85a97f731ded11610053e636d140af806af7
Chris <cfa@google.com>
2020-01-14 21:58:06 +0000

    Add actions, plus a filter and a shortcode

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -26,7 +26,21 @@ final class Plugin {
 
 	/** Adds WordPress actions. */
 	private function add_actions() {
+		// Admin menu.
 		add_action( 'admin_menu', array( $this, 'add_admin_menu_item' ) );
+
+		// Admin page.
+		add_action( 'admin_init', array( $this, 'setup_sections' ) );
+		add_action( 'admin_init', array( $this, 'setup_fields' ) );
+
+		// Post create/edit pages.
+		add_action( 'add_meta_boxes', array( $this, 'setup_post_edit_fields' ) );
+		add_action( 'save_post', array( $this, 'setup_post_save' ) );
+
+		// Post view pages.
+		add_action( 'wp_head', array( $this, 'handle_wp_head' ) );
+		add_filter( 'the_content', array( $this, 'filter_the_content' ) );
+		add_shortcode( 'swg-subscribe', array( $this, 'shortcode_subscribe' ) );
 	}
 
 	/** Adds admin menu item. */
