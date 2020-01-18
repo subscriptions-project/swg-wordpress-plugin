b2810008b5704681fcbc727a2115ed645532917a
Chris <cfa@google.com>
2020-01-18 05:35:34 +0000

    Adds tests for settings_sections

--- tests/phpunit/integration/BasicTest.php
+++ tests/phpunit/integration/BasicTest.php
@@ -28,4 +28,15 @@ class BasicTest extends \WP_UnitTestCase {
 
 		$this->assertNotEmpty( menu_page_url( 'subscribe_with_google', false ) );
 	}
+
+	public function test__admin_page__settings_sections() {
+		global $wp_settings_sections;
+		$this->assertEmpty( $wp_settings_sections );
+		
+		Plugin::$instance->setup_sections();
+		$this->assertEquals( true, is_array( $wp_settings_sections ) );
+		$this->assertEquals( 2, count(
+			$wp_settings_sections['subscribe_with_google']
+		) );
+	}
 }
