f66cacaadf1b7a0e757dcf5513ce732ecc6227ca
Chris <cfa@google.com>
2020-02-28 01:13:56 +0000

    Indents JS with spaces

--- assets/js/test/subscribers.js
+++ assets/js/test/subscribers.js
@@ -3,100 +3,100 @@ import '../subscribers';
 const SUBSCRIBERS = self.SWG[0];
 
 describe('subscribers', () => {
-	let metaEl;
-	let articleEl;
-	let contributeButtonEl;
-	let subscribeButtonEl;
-	let subscriptions;
+  let metaEl;
+  let articleEl;
+  let contributeButtonEl;
+  let subscribeButtonEl;
+  let subscriptions;
 
-	beforeEach(() => {
-		subscriptions = {
-			setOnPaymentResponse: callback => {
-				const response = {
-					complete: () => Promise.resolve(),
-				};
-				callback(Promise.resolve(response));
-			},
-			getEntitlements: () => Promise.resolve({
-				entitlements: [
-					{
-						products: ['basic', 'premium'],
-					},
-				],
-			}),
-			showContributionOptions: jest.fn(),
-			showOffers: jest.fn(),
-		};
+  beforeEach(() => {
+    subscriptions = {
+      setOnPaymentResponse: callback => {
+        const response = {
+          complete: () => Promise.resolve(),
+        };
+        callback(Promise.resolve(response));
+      },
+      getEntitlements: () => Promise.resolve({
+        entitlements: [
+          {
+            products: ['basic', 'premium'],
+          },
+        ],
+      }),
+      showContributionOptions: jest.fn(),
+      showOffers: jest.fn(),
+    };
 
-		metaEl = document.createElement('meta');
-		metaEl.setAttribute('name', 'subscriptions-product-id');
-		metaEl.setAttribute('content', 'premium');
-		document.body.appendChild(metaEl);
+    metaEl = document.createElement('meta');
+    metaEl.setAttribute('name', 'subscriptions-product-id');
+    metaEl.setAttribute('content', 'premium');
+    document.body.appendChild(metaEl);
 
-		articleEl = document.createElement('article');
-		document.body.appendChild(articleEl);
+    articleEl = document.createElement('article');
+    document.body.appendChild(articleEl);
 
-		contributeButtonEl = document.createElement('div');
-		contributeButtonEl.classList.add('swg-contribute-button');
-		contributeButtonEl.dataset.playOffers = 'basic, premium';
-		document.body.appendChild(contributeButtonEl);
+    contributeButtonEl = document.createElement('div');
+    contributeButtonEl.classList.add('swg-contribute-button');
+    contributeButtonEl.dataset.playOffers = 'basic, premium';
+    document.body.appendChild(contributeButtonEl);
 
-		subscribeButtonEl = document.createElement('div');
-		subscribeButtonEl.classList.add('swg-button');
-		subscribeButtonEl.dataset.playOffers = 'basic, premium';
-		document.body.appendChild(subscribeButtonEl);
-	});
+    subscribeButtonEl = document.createElement('div');
+    subscribeButtonEl.classList.add('swg-button');
+    subscribeButtonEl.dataset.playOffers = 'basic, premium';
+    document.body.appendChild(subscribeButtonEl);
+  });
 
-	afterEach(() => {
-		metaEl.remove();
-		articleEl.remove();
-	});
+  afterEach(() => {
+    metaEl.remove();
+    articleEl.remove();
+  });
 
-	it('handles missing meta element', async () => {
-		metaEl.remove();
+  it('handles missing meta element', async () => {
+    metaEl.remove();
 
-		await SUBSCRIBERS(subscriptions);
-		expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
-	});
+    await SUBSCRIBERS(subscriptions);
+    expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
+  });
 
-	it('handles mismatched product in meta element', async () => {
-		metaEl.setAttribute('content', 'exclusive');
+  it('handles mismatched product in meta element', async () => {
+    metaEl.setAttribute('content', 'exclusive');
 
-		await SUBSCRIBERS(subscriptions);
-		expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
-	});
+    await SUBSCRIBERS(subscriptions);
+    expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
+  });
 
-	it('handles missing article element', async () => {
-		articleEl.remove();
+  it('handles missing article element', async () => {
+    articleEl.remove();
 
-		await SUBSCRIBERS(subscriptions);
-		expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
-	});
+    await SUBSCRIBERS(subscriptions);
+    expect(articleEl.classList.contains('swg-entitled')).toBeFalsy();
+  });
 
-	it('marks article as entitled', async () => {
-		await SUBSCRIBERS(subscriptions);
-		expect(articleEl.classList.contains('swg-entitled')).toBeTruthy();
-	});
+  it('marks article as entitled', async () => {
+    await SUBSCRIBERS(subscriptions);
+    expect(articleEl.classList.contains('swg-entitled')).toBeTruthy();
+  });
 
-	it('handles subscribe button clicks', async () => {
-		await SUBSCRIBERS(subscriptions);
+  it('handles subscribe button clicks', async () => {
+    await SUBSCRIBERS(subscriptions);
 
-		subscribeButtonEl.click();
+    subscribeButtonEl.click();
 
-		expect(subscriptions.showOffers.mock.calls).toEqual([[{
-			isClosable: true,
-			skus: ['basic', 'premium'],
-		}]]);
-	});
+    expect(subscriptions.showOffers.mock.calls).toEqual([[{
+      isClosable: true,
+      skus: ['basic', 'premium'],
+    }]]);
+  });
 
-	it('handles contribute button clicks', async () => {
-		await SUBSCRIBERS(subscriptions);
+  it('handles contribute button clicks', async () => {
+    await SUBSCRIBERS(subscriptions);
 
-		contributeButtonEl.click();
+    contributeButtonEl.click();
 
-		expect(subscriptions.showContributionOptions.mock.calls).toEqual([[{
-			isClosable: true,
-			skus: ['basic', 'premium'],
-		}]]);
-	});
+    expect(subscriptions.showContributionOptions.mock.calls).toEqual([[{
+      isClosable: true,
+      skus: ['basic', 'premium'],
+    }]]);
+  });
 });
