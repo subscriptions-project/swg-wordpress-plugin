6a821e5437ea3f2034e8da947f7203df4ec9175f
Chris <cfa@google.com>
2020-04-30 21:25:28 +0000

    Adds DOM util

---
+++ assets/js/utils/dom.js
@@ -0,0 +1,5 @@
+/** Selects a single DOM element. */
+export const $ = document.querySelector.bind(document);
+
+/** Selects multiple DOM elements. */
+export const $$ = document.querySelectorAll.bind(document);
